bf4a9c116a7f621c707bade80fc982595e9ab380
Mladen Ilić <mladenilic480@gmail.com>
2016-08-23 11:13:50 +0200

    Remove php5-snmp
    Fixes error reports caused by missing snmp package

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,7 @@ apt-get install -y apache2
 apt-get install -y php5
 apt-get install -y libapache2-mod-php5
 apt-get install -y php5-mysql php5-curl php5-gd php5-intl php-pear php5-imap php5-mcrypt php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php-apc
+apt-get remove php5-snmp
 
 # Install postfix
 # --------------------
